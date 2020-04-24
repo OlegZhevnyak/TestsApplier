362e922776796be38ab6c1fb75158a1350d96af1
OlegZhevnyak <ffieryice@gmail.com>
2020-04-24 23:26:40 +0300

    Runner for JUnit tests has been added.

---
+++ tests/JUnit4Runner.java
@@ -0,0 +1,11 @@
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({EngineerFieldsPT.class, EngineerExecuteTestPT.class})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JUnit4Runner {
+}
+
